493ca19b14db7d62c4887ceb919636a7dee66a6e
Anastasia <nastyadelonge_1@mail.ru>
2024-07-02 23:18:19 +0300

    Add files via upload

---
+++ new_users_№2.sql
@@ -0,0 +1,17 @@
+--2. Напишите SQL-запрос, который возвращает количество новых пользователей, зарегистрировавшихся в каждый день за последние 7 дней.
+
+with k as(
+	select 
+	u.user_id,
+	u.registration_date,
+	extract(day from u.registration_date) as days
+	from users2 u
+)
+
+select 
+	count(k.user_id) as new_users,
+	k.registration_date,
+	row_number() over(partition by k.days order by k.days desc)
+from k
+where k.registration_date >= CURRENT_DATE - INTERVAL '7 day'
+group by k.days, k.registration_date;
